1b1daa85ab6e125758b9a521ec4d9148bb454842
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-12-12 21:45:15 -0300

    server priorizando conexao

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
+import 'dotenv/config'; 
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
+ 
 import filmesRoutes from './src/routes/filmesRoutes.js';
 import salasRoutes from './src/routes/salasRoutes.js';
 import sessoesRoutes from './src/routes/sessoesRoutes.js';
 import vendasRoutes from './src/routes/vendasRoutes.js';
-
-dotenv.config();
-
+ 
 const app = express();
+ 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI, {dbName: 'cinema'})
-    .then(() => console.log('Conectado ao MongoDB'))
-    .catch((err) => console.error('Erro na conexão:', err.message));
-
+ 
+const mongoUri = process.env.MONGO_URI;
+ 
+mongoose.connect(mongoUri)
+    .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+    .catch((err) => console.error('Erro na conexão com o Banco:', err));
+ 
 app.use('/filmes', filmesRoutes);
 app.use('/salas', salasRoutes);
 app.use('/sessoes', sessoesRoutes);
 app.use('/vendas', vendasRoutes);
-
-const PORT =3000;
-app.listen(PORT, () => {
-    console.log('Servidor rodando em http://localhost:${PORT}');
+ 
+app.get('/', (req, res) => {
+    res.json({ msg: 'API Cinema Rodando!' });
 });
+ 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`)); 
